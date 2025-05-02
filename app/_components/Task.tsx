692df6e409e5491e9c63f0bbc9203fdc0d9a4115
yumunull <yumunull@gmail.com>
2025-05-02 23:32:01 +0800

    frontend

--- app/_components/Task.tsx
+++ app/_components/Task.tsx
@@ -2,22 +2,28 @@
 import Image from "next/image";
 import {twMerge} from "tailwind-merge";
 import clsx from "clsx";
+import {cn} from "@/lib/utils";
 
 export enum EStatus {
-    TODO,
-    IN_PROGRESS,
-    COMPLETED,
-    WONT_DO,
+    TODO = "TODO",
+    IN_PROGRESS = "IN_PROGRESS",
+    COMPLETED = "COMPLETED",
+    WONT_DO = "WONT_DO",
 }
 
-export const Status : {
-   display:string,
-   status:EStatus, 
-}[] = [
-    {display: "In Progress", status: EStatus.IN_PROGRESS},
-    {display: "Completed", status: EStatus.COMPLETED},
-    {display: "Won't do", status: EStatus.WONT_DO},
-]
+
+interface IStatus {
+    display: string,
+    status: EStatus,
+    icon: string,
+}
+
+export const statusMap = new Map<EStatus, IStatus>([
+    [EStatus.IN_PROGRESS, {display: "In Progress", status: EStatus.IN_PROGRESS, icon: "/Time_atack_duotone.svg"}],
+    [EStatus.COMPLETED, {display: "Completed", status: EStatus.COMPLETED, icon: "/Done_round_duotone.svg"}],
+    [EStatus.WONT_DO, {display: "Won't do", status: EStatus.WONT_DO, icon: "/close_ring_duotone.svg"}],
+])
+
 
 export enum EFunctionality {
     NORMAL,
@@ -68,7 +74,7 @@ const Task: React.FC<Props> = ({
             'bg-skin': functionality === EFunctionality.ADD_TASK,
         }
     ))
-    
+
     const iconStyles = twMerge(clsx(
         'bg-white',
         {
@@ -86,6 +92,25 @@ const Task: React.FC<Props> = ({
                 <span className={`grow text-task-title`}>{name}</span>
                 <span className={`text-description-light`}>{description}</span>
             </div>
+
+            <div className={`w-12 h-12 ml-auto`}>
+                <Status status={status}/>
+            </div>
+        </div>
+    )
+}
+
+export const Status = ({status}: { status: EStatus }) => {
+    const styles = cn("bg-transparent", {
+        "bg-orange": status === EStatus.IN_PROGRESS,
+        "bg-green": status === EStatus.COMPLETED,
+        "bg-red": status === EStatus.WONT_DO,
+    })
+    return (
+        <div className={`${styles} w-full h-full flex aspect-square items-center justify-center rounded-xl`}>
+            {statusMap.has(status) ?
+                <Image src={statusMap.get(status)?.icon ?? "" } alt={`status icon`} width={24} height={24}/> : <>{}</>
+            }
         </div>
     )
 }
--- app/_components/TaskWrapper.tsx
+++ app/_components/TaskWrapper.tsx
@@ -1,13 +1,19 @@
-﻿import {Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet"
+﻿import {Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet"
 import React from "react";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Textarea} from "@/components/ui/textarea";
-import {Icons, Status} from "./Task"
-import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
+import {EStatus, Icons, Status, statusMap} from "./Task"
 import {Button} from "@/components/ui/button";
 import Image from "next/image";
 import {ToggleGroup, ToggleGroupItem} from "@/components/ui/toggle-group";
+
+
+const statusList = [
+    EStatus.IN_PROGRESS,
+    EStatus.COMPLETED,
+    EStatus.WONT_DO,
+]
 
 const TaskWrapper = ({children}: React.PropsWithChildren) => {
     return (
@@ -19,7 +25,7 @@ const TaskWrapper = ({children}: React.PropsWithChildren) => {
                 </div>
             </SheetTrigger>
             <SheetContent
-                className={`sm:max-w-[600px] m-4 rounded-lg p-6 font-outfit [&_Label]:text-input-label [&_Label]:text-gray-blue **:placeholder:text-gray-blue`}>
+                className={`sm:max-w-[600px] h-[calc(100%-32px)] my-auto mr-4 rounded-lg p-6 font-outfit [&_Label]:text-input-label [&_Label]:text-gray-blue **:placeholder:text-gray-blue`}>
                 <SheetHeader className={`p-0 text-task-title `}>
                     <SheetTitle>Task details</SheetTitle>
                 </SheetHeader>
@@ -33,16 +39,18 @@ const TaskWrapper = ({children}: React.PropsWithChildren) => {
 
                     <div>
                         <Label htmlFor={"description"}>Description</Label>
-                        <Textarea id={"description"} placeholder={"Enter a short description"} rows={8} className={`resize-none focus-visible:ring-blue`}/>
+                        <Textarea id={"description"} placeholder={"Enter a short description"} rows={8}
+                                  className={`resize-none focus-visible:ring-blue`}/>
                     </div>
 
                     <div>
                         <Label>Icon</Label>
                         <ToggleGroup type={"single"} className={`flex gap-4 rounded-none`}>
                             {Icons.map((icon, index) =>
-                                    <ToggleGroupItem id={index.toString()} value={icon} className={`h-12 aspect-square rounded-lg bg-light-gray`}>
-                                        {icon}
-                                    </ToggleGroupItem>
+                                <ToggleGroupItem key={icon} id={index.toString()} value={icon}
+                                                 className={`h-12 aspect-square rounded-lg bg-light-gray data-[state=on]:bg-yellow`}>
+                                    {icon}
+                                </ToggleGroupItem>
                             )}
                         </ToggleGroup>
                     </div>
@@ -50,19 +58,16 @@ const TaskWrapper = ({children}: React.PropsWithChildren) => {
                     <div>
                         <Label>Status</Label>
                         <ToggleGroup type={"single"} className={`w-full gap-4 grid grid-cols-2`}>
-                            {Status.map((status, index) =>
-                                <ToggleGroupItem value={status.status.toString()}
-                                                 className={`w-full group h-14 flex justify-start rounded-lg p-[0.15em] border-light-gray border-2`}
-                                                 key={status.display}>
-                                    <div
-                                        className={`aspect-square bg-gray-600 rounded-lg h-full flex justify-center items-center`}>
-                                        <Image src={`Add_round_duotone.svg`} alt={`status icon`} width={24}
-                                               height={24}/>
+                            {statusList.map((status, index) =>
+                                <ToggleGroupItem value={status}
+                                                 className={`w-full group h-14 flex justify-start !rounded-2xl p-[0.15em] border-light-gray border-2 data-[state=on]:border-blue`}
+                                                 key={status}>
+
+                                    <div className={`h-full rounded-xl overflow-hidden`}>
+                                        <Status status={status}/>
                                     </div>
 
-                                    <span>
-                                    {status.display}
-                                </span>
+                                    <span>{statusMap.get(status)?.display}</span>
 
                                     <div
                                         className={`group-data-[state=on]:opacity-100 group-data-[state=off]:opacity-0 rounded-full p-1 aspect-square bg-blue flex justify-center items-center ml-auto mr-2`}>
@@ -73,6 +78,16 @@ const TaskWrapper = ({children}: React.PropsWithChildren) => {
                         </ToggleGroup>
                     </div>
                 </div>
+                <SheetFooter className={`flex-row gap-4 ml-auto`}>
+                    <Button className={`bg-gray-blue rounded-full w-28`}>
+                        Delete
+                        <Image src={`Trash.svg`} alt={`delete icon`} width={20} height={20}/>
+                    </Button>
+                    <Button className={`bg-blue rounded-full w-28`}>
+                        Save
+                        <Image src={`Done_round.svg`} alt={`save icon`} width={20} height={20}/>
+                    </Button>
+                </SheetFooter>
             </SheetContent>
         </Sheet>
     )
